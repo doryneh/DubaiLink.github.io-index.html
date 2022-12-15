5a1234de9512dd8011bd0d81f056a022a370c76f
Dory Nehme <doryy.nehme@gmail.com>
2022-12-15 18:13:03 +0200

    finishing the slider top part with animation

--- js/js.js
+++ js/js.js
@@ -1,16 +1,105 @@
-/*Show Passwod*/
-$(document).ready(function(){
-    $('.fa-eye').on('click', function(){
-       var passInput=$(".password");
-       if(passInput.attr('type')==='password')
-         {
-           passInput.attr('type','text');
-       }
-       else{
-          passInput.attr('type','password');
-       }
-   })
-})
+$(".carousel-control-prev").click(function () {
+   $("section.CarouselPart .carousel .carousel-inner .carousel-item.active").css("animation-name", "circle-in-top-right");
+});
+$(".carousel-control-prev").click(function () {
+   if ($('#carousel-item-one').hasClass('active')) {
+      $("#wraper").removeClass();
+      $("#wraper").addClass("two");
+   };
 
+});
+$(".carousel-control-prev").click(function () {
+   if ($('#carousel-item-Two').hasClass('active')) {
+      $("#wraper").removeClass();
+      $("#wraper").addClass("three");
+   };
 
+});
+
+$(".carousel-control-prev").click(function () {
+   if ($('#carousel-item-Three').hasClass('active')) {
+      $("#wraper").removeClass();
+      $("#wraper").addClass("four");
+   };
+});
+$(".carousel-control-prev").click(function () {
+   if ($('#carousel-item-Four').hasClass('active')) {
+      $("#wraper").removeClass();
+      $("#wraper").addClass("five");
+   };
+});
+$(".carousel-control-prev").click(function () {
+   if ($('#carousel-item-Five').hasClass('active')) {
+      $("#wraper").removeClass();
+      $("#wraper").addClass("six");
+   };
+});
+$(".carousel-control-prev").click(function () {
+
+   if ($('#carousel-item-Six').hasClass('active')) {
+      $("#wraper").removeClass();
+      $("#wraper").addClass("seven");
+   };
+});
+
+$(".carousel-control-prev").click(function () {
+   if ($('#carousel-item-Seven').hasClass('active')) {
+      $("#wraper").removeClass();
+      $("#wraper").addClass("one");
+   };
+});
+
+/*******
+ Previous
+ ********/
+ $(".carousel-control-next").click(function () {
+   $("section.CarouselPart .carousel .carousel-inner .carousel-item.active").css("animation-name", "circle-in-top-right");
+});
+$(".carousel-control-next").click(function () {
+   if ($('#carousel-item-one').hasClass('active')) {
+      $("#wraper").removeClass();
+      $("#wraper").addClass("four");
+   };
+
+});
+$(".carousel-control-next").click(function () {
+   if ($('#carousel-item-Two').hasClass('active')) {
+      $("#wraper").removeClass();
+      $("#wraper").addClass("five");
+   };
+
+});
+
+$(".carousel-control-next").click(function () {
+   if ($('#carousel-item-Three').hasClass('active')) {
+      $("#wraper").removeClass();
+      $("#wraper").addClass("six");
+   };
+});
+$(".carousel-control-next").click(function () {
+   if ($('#carousel-item-Four').hasClass('active')) {
+      $("#wraper").removeClass();
+      $("#wraper").addClass("seven");
+   };
+});
+$(".carousel-control-next").click(function () {
+   if ($('#carousel-item-Five').hasClass('active')) {
+      $("#wraper").removeClass();
+      $("#wraper").addClass("one");
+   };
+});
+$(".carousel-control-next").click(function () {
+
+   if ($('#carousel-item-Six').hasClass('active')) {
+      $("#wraper").removeClass();
+      $("#wraper").addClass("two");
+   };
+});
+
+$(".carousel-control-next").click(function () {
+   if ($('#carousel-item-Seven').hasClass('active')) {
+      $("#wraper").removeClass();
+      $("#wraper").addClass("three");
+   };
+});
 
