49438e3c47630fb583a77aa17db6befb7f328b2f
Dory Nehme <doryy.nehme@gmail.com>
2022-12-19 18:50:29 +0200

    first commit

--- js/js.js
+++ js/js.js
@@ -1,52 +1,79 @@
-
-$(".carousel-control-prev,.carousel-control-next").click(function () {
-   $("section.CarouselPart .carousel .carousel-inner .carousel-item.active").css("animation-name", " circle-in-top-right");
+$("#carouselExampleControls .carousel-control-prev,#carouselExampleControls .carousel-control-next").click(function () {
+   $("#carouselExampleControls .carousel-item.active").css("animation-name", " circle-in-top-right");
 });
-$(".carousel-control-prev").click(function () {
+$("#carouselExampleControls .carousel-control-prev").click(function () {
    if ($('#carousel-item-one').hasClass('active')) {
       $("#wraper").removeClass();
       $("#wraper").addClass("two");
-   };
+      $("#NIU").addClass("d-none");
+      $("#NIU").removeClass("d-block");
+      $("#LandingPage").addClass("d-block");
+      $("#LandingPage").removeClass("d-none");
 
+   };
 });
-$(".carousel-control-prev").click(function () {
+$("#carouselExampleControls .carousel-control-prev").click(function () {
    if ($('#carousel-item-Two').hasClass('active')) {
       $("#wraper").removeClass();
       $("#wraper").addClass("three");
+      $("#LandingPage").addClass("d-none");
+      $("#LandingPage").removeClass("d-block");
+      $("#DubaiLink").addClass("d-block");
+      $("#DubaiLink").removeClass("d-none");
    };
 
 });
 
-$(".carousel-control-prev").click(function () {
+$("#carouselExampleControls .carousel-control-prev").click(function () {
    if ($('#carousel-item-Three').hasClass('active')) {
       $("#wraper").removeClass();
       $("#wraper").addClass("four");
+      $("#DubaiLink").addClass("d-none");
+      $("#DubaiLink").removeClass("d-block");
+      $("#GTE").addClass("d-block");
+      $("#GTE").removeClass("d-none");
    };
 });
-$(".carousel-control-prev").click(function () {
+$("#carouselExampleControls .carousel-control-prev").click(function () {
    if ($('#carousel-item-Four').hasClass('active')) {
       $("#wraper").removeClass();
       $("#wraper").addClass("five");
+      $("#GTE").addClass("d-none");
+      $("#GTE").removeClass("d-block");
+      $("#Royale").addClass("d-block");
+      $("#Royale").removeClass("d-none");
    };
 });
-$(".carousel-control-prev").click(function () {
+$("#carouselExampleControls .carousel-control-prev").click(function () {
    if ($('#carousel-item-Five').hasClass('active')) {
       $("#wraper").removeClass();
       $("#wraper").addClass("six");
+      $("#Royale").addClass("d-none");
+      $("#Royale").removeClass("d-block");
+      $("#STS").addClass("d-block");
+      $("#STS").removeClass("d-none");
    };
 });
-$(".carousel-control-prev").click(function () {
+$("#carouselExampleControls .carousel-control-prev").click(function () {
 
    if ($('#carousel-item-Six').hasClass('active')) {
       $("#wraper").removeClass();
       $("#wraper").addClass("seven");
+      $("#STS").addClass("d-none");
+      $("#STS").removeClass("d-block");
+      $("#Tripstik").addClass("d-block");
+      $("#Tripstik").removeClass("d-none");
    };
 });
 
-$(".carousel-control-prev").click(function () {
+$("#carouselExampleControls .carousel-control-prev").click(function () {
    if ($('#carousel-item-Seven').hasClass('active')) {
       $("#wraper").removeClass();
       $("#wraper").addClass("one");
+      $("#Tripstik").addClass("d-none");
+      $("#Tripstik").removeClass("d-block");
+      $("#NIU").addClass("d-block");
+      $("#NIU").removeClass("d-none");
    };
 });
 
@@ -55,54 +82,82 @@ $(".carousel-control-prev").click(function () {
 /*******
  Previous
  ********/
-$(".carousel-control-next").click(function () {
+$("#carouselExampleControls .carousel-control-next").click(function () {
    $("section.CarouselPart .carousel .carousel-inner .carousel-item.active").css("animation-name", "circle-in-top-right");
 });
-$(".carousel-control-next").click(function () {
+$("#carouselExampleControls .carousel-control-next").click(function () {
    if ($('#carousel-item-one').hasClass('active')) {
       $("#wraper").removeClass();
       $("#wraper").addClass("four");
+      $("#DubaiLink").addClass("d-none");
+      $("#DubaiLink").removeClass("d-block");
+      $("#LandingPage").addClass("d-block");
+      $("#LandingPage").removeClass("d-none");
    };
 
 });
-$(".carousel-control-next").click(function () {
+$("#carouselExampleControls .carousel-control-next").click(function () {
    if ($('#carousel-item-Two').hasClass('active')) {
       $("#wraper").removeClass();
       $("#wraper").addClass("five");
+      $("#GTE").addClass("d-none");
+      $("#GTE").removeClass("d-block");
+      $("#DubaiLink").addClass("d-block");
+      $("#DubaiLink").removeClass("d-none");
    };
 
 });
 
-$(".carousel-control-next").click(function () {
+$("#carouselExampleControls .carousel-control-next").click(function () {
    if ($('#carousel-item-Three').hasClass('active')) {
       $("#wraper").removeClass();
       $("#wraper").addClass("six");
+      $("#Royale").addClass("d-none");
+      $("#Royale").removeClass("d-block");
+      $("#GTE").addClass("d-block");
+      $("#GTE").removeClass("d-none");
    };
 });
-$(".carousel-control-next").click(function () {
+$("#carouselExampleControls .carousel-control-next").click(function () {
    if ($('#carousel-item-Four').hasClass('active')) {
       $("#wraper").removeClass();
       $("#wraper").addClass("seven");
+      $("#STS").addClass("d-none");
+      $("#STS").removeClass("d-block");
+      $("#Royale").addClass("d-block");
+      $("#Royale").removeClass("d-none");
    };
 });
-$(".carousel-control-next").click(function () {
+$("#carouselExampleControls .carousel-control-next").click(function () {
    if ($('#carousel-item-Five').hasClass('active')) {
       $("#wraper").removeClass();
       $("#wraper").addClass("one");
+      $("#Tripstik").addClass("d-none");
+      $("#Tripstik").removeClass("d-block");
+      $("#STS").addClass("d-block");
+      $("#STS").removeClass("d-none");
    };
 });
-$(".carousel-control-next").click(function () {
+$("#carouselExampleControls .carousel-control-next").click(function () {
 
    if ($('#carousel-item-Six').hasClass('active')) {
       $("#wraper").removeClass();
       $("#wraper").addClass("two");
+      $("#NIU").addClass("d-none");
+      $("#NIU").removeClass("d-block");
+      $("#Tripstik").addClass("d-block");
+      $("#Tripstik").removeClass("d-none");
    };
 });
 
-$(".carousel-control-next").click(function () {
+$("#carouselExampleControls .carousel-control-next").click(function () {
    if ($('#carousel-item-Seven').hasClass('active')) {
       $("#wraper").removeClass();
       $("#wraper").addClass("three");
+      $("#NIU").removeClass("d-none");
+      $("#NIU").addClass("d-block");
+      $("#LandingPage").removeClass("d-block");
+      $("#LandingPage").addClass("d-none");
    };
 });
 
@@ -113,7 +168,7 @@ $(window).scroll(function () {
       $(".Logo").css("filter", " unset");
 
    }
-   else{
+   else {
       $(".Logo").css("filter", "  brightness(0) invert(1)");
    }
    if ($(this).scrollTop() == 0) {
@@ -195,82 +250,84 @@ Time Line
 *********/
 var element = $(".GreenText2");
 var element2 = $(".GreenText3");
-var previous =$(".GreenText1")
+var previous = $(".GreenText1")
 var elementOffset = element.offset().top;
 var elementOffset2 = element2.offset().top;
 var previousOffset = previous.offset().top;
 var windowHeight = $(window).height();
-   var windowScroll = $(window).scrollTop();
-$(window).scroll(function() {
-  
+var windowScroll = $(window).scrollTop();
+$(window).scroll(function () {
 
-   
- 
+
+
+
    if (elementOffset < windowHeight + windowScroll) {
-     setTimeout(function() {
-       element.addClass("GreenText");
-       previous.removeClass("GreenText");
+      setTimeout(function () {
+         element.addClass("GreenText");
+         previous.removeClass("GreenText");
 
-     }, 3000);
+      }, 3000);
    }
-   else{
+   else {
       element.removeClass("GreenText");
       previous.addClass("GreenText");
 
    }
    if (elementOffset2 < windowHeight + windowScroll) {
-      setTimeout(function() {
-        element2.addClass("GreenText");
-        element.removeClass("GreenText");
- 
+      setTimeout(function () {
+         element2.addClass("GreenText");
+         element.removeClass("GreenText");
+
       }, 6000);
-    }
-    else{
-       element2.removeClass("GreenText");
-       previous.addClass("GreenText");
- 
-    }
-    if (previousOffset < windowHeight + windowScroll) {
-      setTimeout(function() {
-        previous.addClass("GreenText");
-        element2.removeClass("GreenText");
- 
+   }
+   else {
+      element2.removeClass("GreenText");
+      previous.addClass("GreenText");
+
+   }
+   if (previousOffset < windowHeight + windowScroll) {
+      setTimeout(function () {
+         previous.addClass("GreenText");
+         element2.removeClass("GreenText");
+
       }, 9000);
-     
-    }
-    
- });
+
+   }
+
+});
 
 /*Carousel Brands slide time*/
-$(document).ready(function() {
-$('#carouselExampleSlidesOnly').carousel({
-   interval: 100
- });
- $('#carouselDubai,#TextCarousel,#TextCarouselNIU,#carouselNIU').carousel({
-   interval: false
- });
- $('.carousel').carousel('pause');
-
+$(document).ready(function () {
+   $('#carouselExampleSlidesOnly').carousel({
+      interval: 100
+   });
+   $('#carouselDubai,#TextCarousel,#TextCarouselNIU,#carouselNIU').carousel({
+      interval: false
+   });
+   $('#carouselExampleControls').carousel('pause');
+   $('#carouselExampleControls').carousel({
+      touch: false
+   });
 });
 /********
  Control 2 carousel in Dubai Link by the same buttons
  *****/
- $('#Next').on('click', function(e) {
+$('#Next').on('click', function (e) {
    e.preventDefault()
    $('#TextCarousel').carousel('prev')
- })
- $('#Prev').on('click', function(e) {
+})
+$('#Prev').on('click', function (e) {
    e.preventDefault()
    $('#TextCarousel').carousel('next')
- })
- $('#NextNIU').on('click', function(e) {
+})
+$('#NextNIU').on('click', function (e) {
    e.preventDefault()
    $('#TextCarouselNIU').carousel('prev')
- })
- $('#PrevNIU').on('click', function(e) {
+})
+$('#PrevNIU').on('click', function (e) {
    e.preventDefault()
    $('#TextCarouselNIU').carousel('next')
- })
+})
 /********
 Hide and show the page content depending on the carousel
  ****/
@@ -279,7 +336,7 @@ Hide and show the page content depending on the carousel
    //    $('#LandingPage').removeClass("d-none");
    //    $('#LandingPage').addClass("d-block");
    //    $('#DubaiLink').addClass("d-none");
-   // } 
+   // }
    // if (!$('#carousel-item-one').hasClass('active')){
    //    $('#LandingPage').removeClass("d-block");
    //    $('#LandingPage').addClass("d-none");
